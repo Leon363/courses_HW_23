77122fec75d6a35d55658b8ab35e692c4b21f40e
Leon363 <tuea1982@gmail.com>
2022-03-11 18:25:38 +0200

    HW-24

--- src/ui/form_handler.js
+++ src/ui/form_handler.js
@@ -1,7 +1,7 @@
 export default class FormHandler {
-    #formElement;
-    #alertElement;
-    #inputElements;
+    #formElement
+    #alertElement
+    #inputElements
     constructor(idForm, idAlert) {
         this.#formElement = document.getElementById(idForm);
         this.#alertElement = document.getElementById(idAlert);
@@ -10,21 +10,36 @@ export default class FormHandler {
     addHandler(fnProcessor) {
         this.#formElement.addEventListener('submit', event => {
             event.preventDefault();
-            const data = Array.from(this.#inputElements).reduce((obj, element) => {
+            const data = Array.from(this.#inputElements)
+            .reduce((obj, element) => {
                 obj[element.name] = element.value;
                 return obj;
             }, {})
-            
             const message = fnProcessor(data);
-            if(!message) {
-                this.#formElement.reset();
+            if (!message) {
+                this.#formElement.reset(); //everything ok
                 this.#alertElement.innerHTML = '';
             } else {
-               this.#alertElement.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-               <strong>ERROR!!! </strong>${message}
-               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-             </div>`;
+                const alert = `
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> ${message}.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>`;
+            this.#alertElement.innerHTML = alert;
             }
         })
     }
+    fillOptions(idOptions, options ) {
+        document.getElementById(idOptions).innerHTML +=
+        `${getOptions(options)}`
+    }
+    show() {
+        this.#formElement.hidden = false;
+    }
+    hide() {
+        this.#formElement.hidden = true;
+    }
+}
+function getOptions(options) {
+    return options.map(o => `<option value="${o}">${o}</option>`).join('');
 }
